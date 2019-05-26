61e8280176c90c8029d1f5a75d3deb259d644e85
finnagin <flan313@gmail.com>
2019-05-26 12:40:15 -0700

    got adaboost working

--- Project 2/test_interim.R
+++ Project 2/test_interim.R
@@ -1,7 +1,11 @@
 library(OpenImageR)
 library(tidyverse)
+
 library(e1071)
 library(randomForest)
+
+library(fastAdaboost)
+library(gbm)
 
 
 # Generate files paths
@@ -53,9 +57,12 @@ for(file in train_files){
   i <- i+1
 }
 colnames(X)=cn
-X <- as_tibble(X,colnames=cn)
-X <- X %>%
-  mutate(y = factor(y))
+#X <- as_tibble(X,colnames=cn)
+#X <- X %>%
+#  mutate(y = factor(y))
+
+X <- as.data.frame(X)
+X$y <- as.factor(X$y)
 
 set.seed(Sys.time())
 train_idx <- sample(1:nrow(X),11200)
@@ -113,3 +120,61 @@ svm_pred_0<-predict(svm_model,filter(X[-train_idx,],y=="0"))
 svm_err<- with(X[-train_idx,],mean(y!=svm_pred))
 svm_err_1<- with(filter(X[-train_idx,],y=="1"),mean(y!=svm_pred_1))
 svm_err_0<- with(filter(X[-train_idx,],y=="0"),mean(y!=svm_pred_0))
+
+######################################################
+#                      Week 2                        #
+######################################################
+
+pc <- prcomp(select(X,-y))  # Eigen decomposition of Sample Covariance
+pc1 <- prcomp(select(X,-y), scale. = T)  # Eigen decomposition of Sample Covariance
+
+summary(pc)
+
+Xpc = as_tibble(pc$x[, 1:55])
+Xpc$y = X$y
+
+## work around bug in gbm 2.1.1
+predict.gbm <- function (object, newdata, n.trees, type = "link", single.tree = FALSE, ...) {
+  if (missing(n.trees)) {
+    if (object$train.fraction < 1) {
+      n.trees <- gbm.perf(object, method = "test", plot.it = FALSE)
+    }
+    else if (!is.null(object$cv.error)) {
+      n.trees <- gbm.perf(object, method = "cv", plot.it = FALSE)
+    }
+    else {
+      n.trees <- length(object$train.error)
+    }
+    cat(paste("Using", n.trees, "trees...\n"))
+    gbm::predict.gbm(object, newdata, n.trees, type, single.tree, ...)
+  }
+}
+
+X2 <- as.data.frame(X)
+
+
+X2$y <- as.character(X2$y)
+
+adb <- gbm(y~.,data=X2[train_idx,], distribution = "bernoulli", n.trees = 500)
+adb_pred<-predict(adb,X2[-train_idx,],n.trees=100, type = "response")
+adb_pred_1<-predict(adb,filter(X2[-train_idx,],y=="1"),n.trees=500, type = "response")
+adb_pred_0<-predict(adb,filter(X2[-train_idx,],y=="0"),n.trees=500, type = "response")
+
+class_con <- function(x){
+  if(x >= .5){
+    return("1")
+  }
+  if(x < .5){
+    return("0")
+  }
+}
+
+adb_pred <- lapply(adb_pred, class_con)
+adb_pred_1 <- lapply(adb_pred_1, class_con)
+adb_pred_0 <- lapply(adb_pred_0, class_con)
+
+adb_err<- with(X[-train_idx,],mean(y!=adb_pred))
+adb_err_1<- with(filter(X[-train_idx,],y=="1"),mean(y!=adb_pred_1))
+adb_err_0<- with(filter(X[-train_idx,],y=="0"),mean(y!=adb_pred_0))
+
+adb_pred
